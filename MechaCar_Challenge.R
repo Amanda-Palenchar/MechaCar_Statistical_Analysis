6ce3c5f8d9381bbe2a61ffe6edc798205531c27b
Amanda Palenchar <amanda.palenchar@gmail.com>
2022-10-01 14:08:47 -0600

    Added deliverable 3.

--- MechaCar_Challenge.R
+++ MechaCar_Challenge.R
@@ -15,3 +15,19 @@ Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFa
 total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+# Deliverable 3
+
+t.test(Suspension_Coil$PSI, mu=1500) # t.test for total sample pop vs population
+
+Lot1 <- subset(Suspension_Coil, Manufacturing_Lot=="Lot1")
+
+t.test(Lot1$PSI, mu=1500) # t.test for lot 1 vs population
+
+Lot2 <- subset(Suspension_Coil, Manufacturing_Lot=="Lot2")
+
+t.test(Lot2$PSI, mu=1500) # t.test for lot 2 vs population
+
+Lot3 <- subset(Suspension_Coil, Manufacturing_Lot=="Lot3")
+
+t.test(Lot3$PSI, mu=1500) # t.test for lot 3 vs population
